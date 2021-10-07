558ee17c96df53777c698f69252886993fc23fad
Phạm Anh Đức <pham.anh.duc@vbpo.com.vn>
2021-10-07 15:09:11 +0700

    update select ticket and choose number

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -47,11 +47,11 @@ const Home = () => {
     const openBoxAction = useOpenBoxAction();
     const {approve, isApprove} = useERC20Action();
     const [isApproved, setIsApproved] = useState(false);
-    const [price, setPrice] = useState(null);
+    const [price, setPrice] = useState(10000);
     const [amount, setAmount] = useState(1);
     const [nextDraw, setNextDraw] = useState(null);
     const [loading, setLoading] = useState(false);
-    const [lsTicket,setLsTicket] = useState([[1,2,3,4,5,6]])
+    const [lsTicket,setLsTicket] = useState([getRandomTicket()])
     const {account} = useWallet();
     // useEffect(() => {
     //     openBoxAction.getSeedPrice()
@@ -68,61 +68,69 @@ const Home = () => {
         const [lsNumber, setLsNumber] = useState(lsTicket[indexTicket])
         const newLsTicket = [...lsTicket]
         const onInputChange = (e,index) => {
-            console.log(e.target.value)
+            if (e.target.value) {
             let currentValue = [...lsNumber]
             currentValue[index] = parseInt(e.target.value)
             setLsNumber(currentValue)
             newLsTicket[indexTicket] = currentValue
-            setLsTicket(newLsTicket)
+            setLsTicket(newLsTicket)}
         }
-        console.log(lsNumber)
         return (
             <>
-            {lsNumber.map((item, index) =>
-                <>
-                <li>
-                    <span><input onChange={(e)=>onInputChange(e,index)} value={lsNumber[index]} min={0} max={9}/></span>
-                </li>
-
-                </>
-                    )}
-                <li>
-                    <button className="custom-button1" onClick={()=>setLsNumber(getRandomTicket())}><i className="fas fa-magic"></i>Quick
-                        Pick</button>
-                </li>
-                <li>
-                    <button className="custom-button2" onClick={()=>callbackRemoveTicket()}>Remove</button>
-                </li>
-            </>
-        )
-    }
-
-    const BoxTicket = ({lsTicket,callbackRemoveTicket}) => {
-        return (
-            <>
-            {lsTicket.map((item,index) =>
-            <div className="col-lg-6 col-md-6 mt-1">
+                <div className={lsTicket.length > 1 ? "col-lg-6 col-md-6 mb-5" : "col-lg-12 col-md-12 mb-5"}  key={'boxsingle' + indexTicket}>
                 <div className="single-pick">
                     <div className="header-area">
                         <h4 className="title">Pick 6 Numbers</h4>
                         <div className="buttons">
-                            <a href="#" className="custom-button1"><i className="fas fa-magic"></i>Quick
-                                Pick</a>
-                            <a href="#" className="custom-button2"><i className="fas fa-trash-alt"></i>Clear
-                                All</a>
+                            <button className="custom-button1" onClick={()=>setLsNumber(getRandomTicket())}><i className="fas fa-magic"></i>Quick
+                            Pick</button>
+                            {lsTicket.length > 1 ? <button className="custom-button2" onClick={()=>callbackRemoveTicket(indexTicket)}>Remove</button> :null }
                         </div>
                     </div>
                     <div className="body-area">
                         <ul>
-                            <BoxSingleTicket lsTicket={lsTicket} callbackRemoveTicket={()=>callbackRemoveTicket()} indexTicket={index}/>
-                        </ul>
+            {lsNumber.map((item, index) =>
+                <div key={'abc'+index} style={{display:'inline-block'}}>
+                <li>
+                    <span><input className={'input-select-lot'} maxLength={'1'} pattern="[0-9]{1}" onChange={(e)=>onInputChange(e,index)} value={lsNumber[index]} min={0} max={9}/></span>
+                </li>
+
+                </div>
+                    )}
+                    </ul>
                     </div>
                 </div>
             </div>
+            </>
+        )
+    }
+    const container = document.getElementsByClassName('input-select-lot');
+      for (let i = 0;i < container.length; i++) {
+          container[i].addEventListener('keyup',function (e) {
+              if (['0','1','2','3','4','5','6','7','8','9'].includes(e.key)){
+              this.value = e.key}
+          })
+      }
+    const BoxTicket = ({lsTicket,callbackRemoveTicket}) => {
+        return (
+            <>
+            {lsTicket.map((item,index) =>
+
+                <BoxSingleTicket lsTicket={lsTicket} callbackRemoveTicket={(indexTicket)=>callbackRemoveTicket(indexTicket)} indexTicket={index} key={'box' + index}/>
+
             )}
         </>
         )
     }
+      useEffect(()=> {
+          const container = document.getElementsByClassName('input-select-lot');
+          for (let i = 0;i < container.length; i++) {
+              container[i].addEventListener('keyup',function (e) {
+              if (['0','1','2','3','4','5','6','7','8','9'].includes(e.key)){
+                this.value = e.key}
+              })
+          }
+      },[lsTicket])
     useEffect(()=>{
         const currentTime = new Date();
         const nextDate = new Date(new Date().setDate(currentTime.getDate()+1))
@@ -147,7 +155,7 @@ const Home = () => {
     const addTicket = () => {
         const currentLsTicket = [...lsTicket]
         if (currentLsTicket.length < 6) {
-            currentLsTicket.push(currentLsTicket[currentLsTicket.length - 1] + 1)
+            currentLsTicket.push(getRandomTicket())
             setLsTicket(currentLsTicket)
         }
     }
@@ -181,12 +189,15 @@ const Home = () => {
     }
     const RemoveTicket = (index) => {
         const currentLsTicket = [...lsTicket]
+        currentLsTicket.splice(index,1)
+        setLsTicket(currentLsTicket)
     }
     console.log("lsTicket",lsTicket)
     return (
         <>
         <section className="breadcrumb-area">
             <img className="lottory" src="assets/images/lottery-b-icon.png" alt=""/>
+
                 <div className="container">
                     <div className="content">
                         <h2 className="title">
@@ -253,7 +264,7 @@ const Home = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-lg-9">
-                        <div className="row justify-content-center">
+                        <div className="row justify-content-center" id={'test'}>
                             <BoxTicket lsTicket={lsTicket} callbackRemoveTicket={(index)=>RemoveTicket(index)}/>
                         </div>
                         <div className="row">
@@ -278,19 +289,19 @@ const Home = () => {
                             <div className="middle-area">
                                 <div className="tikit">
                                     <span className="left">Filled out Tickets</span>
-                                    <span className="right">3</span>
+                                    <span className="right">{lsTicket.length}</span>
                                 </div>
                                 <div className="price">
                                     <span className="left">Ticket Price
-                                        <small>(8 tickets <i className="fas fa-times"></i> $4.99)</small>
+                                        <small>({lsTicket.length} tickets <i className="fas fa-times"></i> ${price})</small>
                                     </span>
-                                    <span className="right">$39.92</span>
+                                    <span className="right">${lsTicket.length*price}</span>
                                 </div>
                             </div>
                             <div className="bottom-area">
                                 <div className="total-area">
                                     <span className="left">Total</span>
-                                    <span className="right">$39.92</span>
+                                    <span className="right">${lsTicket.length*price}</span>
                                 </div>
                                 <a href="#" className="custom-button2">Buy Tickets</a>
                             </div>
